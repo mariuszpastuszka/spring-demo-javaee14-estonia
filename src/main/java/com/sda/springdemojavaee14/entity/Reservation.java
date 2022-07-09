6148168bb2fee4ab8e068c6a1bb437886cf1650a
maniek <mpastuszka@gmail.com>
2022-07-09 10:45:34 +0200

    [add] - configuration of in memory h2 database for single Reservation entity

--- src/main/java/com/sda/springdemojavaee14/entity/Reservation.java
+++ src/main/java/com/sda/springdemojavaee14/entity/Reservation.java
@@ -5,13 +5,22 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.time.LocalDateTime;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "RESERVATIONS")
 public class Reservation {
+    @Id // primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
 
     Long tableNumber;
